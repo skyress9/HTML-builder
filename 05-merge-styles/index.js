6953f020ecfeb948d99295fa7da66bcb9e6b08da
skyress9 <lion.super2@gmail.com>
2021-11-07 02:34:49 +0300

    refactor: index

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,19 +1,20 @@
 const path = require('path');
 const fsPromises = require('fs/promises');
 
-async function getStyles() {
+const stylesPath = path.join(__dirname, 'styles');
+const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
+
+async function getStyles(source, bundle) {
   let array = [];
-  const stylesPath = path.join(__dirname, 'styles');
-  const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
-  const files = await fsPromises.readdir(stylesPath, {withFileTypes: true});
+  const files = await fsPromises.readdir(source, {withFileTypes: true});
   files.forEach(file => {
     if (file.isFile() && path.extname(file.name) == '.css') {
-      fsPromises.readFile(path.join(stylesPath, file.name), 'utf-8').then((data) => {
+      fsPromises.readFile(path.join(source, file.name), 'utf-8').then((data) => {
         array.push(data);
-        fsPromises.writeFile(bundlePath, array.join(''));
+        fsPromises.writeFile(bundle, array.join(''));
       });
     }
   });
 }
 
-getStyles();
+getStyles(stylesPath, bundlePath);
